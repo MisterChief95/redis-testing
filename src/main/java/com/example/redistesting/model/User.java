d258ba60d9fad027232dadb006b1d4aadc24925c
brendan <brendan.lackey@outlook.com>
2022-01-13 10:57:20 -0700

    Add Jackson annotations

--- src/main/java/com/example/redistesting/model/User.java
+++ src/main/java/com/example/redistesting/model/User.java
@@ -1,5 +1,9 @@
 package com.example.redistesting.model;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(builder = User.Builder.class)
 public class User {
     private final String id;
     private final String name;
@@ -23,21 +27,24 @@ public class User {
         return age;
     }
 
-    private static final class Builder {
+    public static final class Builder {
         private String id;
         private String name;
         private int age;
 
+        @JsonSetter("id")
         public Builder withId(String id){
             this.id = id;
             return this;
         }
 
+        @JsonSetter("name")
         public Builder withName(String name){
             this.name = name;
             return this;
         }
 
+        @JsonSetter("age")
         public Builder withAge(int age){
             this.age = age;
             return this;
