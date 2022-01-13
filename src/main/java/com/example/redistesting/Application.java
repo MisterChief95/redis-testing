214e28bea43e274b178c3be944d7abb084a9a6d1
brendan <brendan.lackey@outlook.com>
2022-01-13 11:04:29 -0700

    Just use quick and dirty Spring runner

--- src/main/java/com/example/redistesting/Application.java
+++ src/main/java/com/example/redistesting/Application.java
@@ -1,19 +1,11 @@
 package com.example.redistesting;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .bannerMode(Banner.Mode.OFF)
-                .headless(true)
-                .web(WebApplicationType.SERVLET)
-                .build(args)
-                .run();
+        SpringApplication.run(Application.class, args);
     }
 }
