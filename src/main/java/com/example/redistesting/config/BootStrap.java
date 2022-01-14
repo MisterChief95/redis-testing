6069a2e7d4fb7a9c8ce4b76feca7e56c64fd851e
brendan <brendan.lackey@outlook.com>
2022-01-14 14:06:46 -0700

    * Remove logFilter * add cacheService * Inject RedisReactiveCommands

--- src/main/java/com/example/redistesting/config/BootStrap.java
+++ src/main/java/com/example/redistesting/config/BootStrap.java
@@ -2,31 +2,26 @@
 package com.example.redistesting.config;
 
 import com.example.redistesting.contract.CacheRepository;
+import com.example.redistesting.contract.CacheService;
 import com.example.redistesting.model.User;
 import com.example.redistesting.repository.CacheRepositoryImpl;
-import io.lettuce.core.api.async.RedisAsyncCommands;
+import com.example.redistesting.service.CacheServiceImpl;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 @Import({RedisConfiguration.class, CommonBeans.class})
 public class BootStrap {
 
   @Bean
-  public CacheRepository cacheRepository(RedisAsyncCommands<String, User> commands) {
+  public CacheRepository cacheRepository(RedisReactiveCommands<String, User> commands) {
     return new CacheRepositoryImpl(commands);
   }
 
   @Bean
-  public CommonsRequestLoggingFilter logFilter() {
-    var filter = new CommonsRequestLoggingFilter();
-    filter.setIncludeQueryString(true);
-    filter.setIncludePayload(true);
-    filter.setMaxPayloadLength(10000);
-    filter.setIncludeHeaders(false);
-    filter.setAfterMessagePrefix("REQUEST DATA : ");
-    return filter;
+  public CacheService cacheService(CacheRepository cacheRepository) {
+    return new CacheServiceImpl(cacheRepository);
   }
 }
